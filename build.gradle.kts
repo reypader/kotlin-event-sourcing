e73f16647681a55ac481e72261c64c2ad0e4145f
rmpader <reynaldmpader@gmail.com>
2025-10-29 15:20:48 +0800

    Initial commit
    Project Setup

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+group = "com.rmpader"
+version = "1.0-SNAPSHOT"
+description = "kotlin-event-sourcing"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.kotlinx.json)
+    // Test
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
